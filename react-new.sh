cb756fc08a52b6751144ee206d9a3c72170e0edf
tyanakaz <xe314m@gmail.com>
2018-10-31 12:08:51 +0900

    Delete unnessesary files

--- react-new.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd .. 
-ember new myapp
-cp -a myapp/. app >/dev/null 2>&1
-rm -rf myapp
--- react-rm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-rm -rf app bower_components config ember-cli-build.js node_modules public ssl testem.js tests tmp vendor .editorconfig .ember-cli .eslintignore .eslintrc.js .travis.yml .watchmanconfig dist package-lock.json
