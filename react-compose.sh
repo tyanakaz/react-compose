8d0ba8990f29c825e61a0a94232e0f324e4f83d1
tyanakaz <xe314m@gmail.com>
2018-10-31 15:51:33 +0900

    Fix react-compose.sh

--- react-compose.sh
+++ react-compose.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 docker-compose run frontend npm init
 docker-compose run frontend npm install --save npm
+docker-compose run frontend npm install gulp browserify reactify vinyl-source-stream --save 
 docker-compose build --force-rm
 docker-compose run frontend npx create-react-app app --force-rm
 docker-compose up -d
