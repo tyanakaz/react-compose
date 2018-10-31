2b2b391eda77b082c2c0afde26cd8a1b98924998
tyanakaz <xe314m@gmail.com>
2018-10-31 15:27:38 +0900

    Fix react-compose.sh

--- react-compose.sh
+++ react-compose.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
+docker-compose run frontend npm init
+docker-compose run frontend npm install --save npm
 docker-compose build --force-rm
 docker-compose run frontend npx create-react-app app --force-rm
 docker-compose up -d
